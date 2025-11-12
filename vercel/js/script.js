3629bf9c32e9ae95639a214e242b9bd0f718542a
jatulcanaza <tulcanazajuan6@gmail.com>
2025-11-11 22:27:22 -0500

    Revert main to only BackBlaze, remove netlify, railway, vercel

--- vercel/js/script.js
+++
@@ -1,5 +0,0 @@
-// Logs a message confirming that the page loaded successfully
-console.log('▲ Vercel Hello World - Loaded Successfully!');
-
-// Logs a message showing that the site is hosted on Vercel
-console.log('🚀 Hosted on Vercel');
